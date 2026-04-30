ea62c00d844922f7698725bc8cceb58f827fe806
CleaningService <Work@DESKTOP-KJQS8H3>
2026-04-30 19:04:07 +0300

    початок роботи над формою статистики може буде отака прикалюха — using System.Windows.Forms.DataVisualization.Charting;

---
+++ CleaningService/Forms/Statistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CleaningService.Forms
+{
+    public partial class Statistics: Form
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Honeydew;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+        }
+    }
+}
